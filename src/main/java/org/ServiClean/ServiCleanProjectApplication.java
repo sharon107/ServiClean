3582fd206b5e02bac22ca6fb2a971f0c1732368c
Josue <josue.martinez7645@gmail.com>
2025-07-31 10:45:51 -0600

    PUInicializacion del proyecto

---
+++ src/main/java/org/ServiClean/ServiCleanProjectApplication.java
@@ -0,0 +1,13 @@
+package org.ServiClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiCleanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiCleanProjectApplication.class, args);
+	}
+
+}
